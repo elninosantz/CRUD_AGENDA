9224df820e1dde96c3f658c25c4c8fa38246a23c
elninosantz <tiaugustocesar@gmail.com>
2025-12-03 22:15:01 -0300

    feature: add function import, export, loading e salve

--- agenda.py
+++ agenda.py
@@ -47,36 +47,26 @@ def adcionar_dados():
     salva em variaveis o valor dos dados do contato
     :return: str(telefone, email, endereco)
     """
+    contato = input('Digite o nome do contato: ')
     telefone = input('Digite o telefone: ').strip()
     email = input('Digite o email: ').strip()
     endereco = input('Digite o endereco: ').strip()
-    return telefone, email, endereco
+    return contato, telefone, email, endereco
 
 
-def dados_contatos(contato_):
-    """
-    adiciona os valores retornados do contato em um dict
-    :param contato_:str
-    :return: none
-    """
-    telefone, email, endereco = adcionar_dados()
-    AGENDA[contato_] = {
-        'telefone': telefone,
-        'email': email,
-        'endereco': endereco,
-    }
-
-
-def incluir_contato():
+def incluir_contato(contato_, telefone, email, endereco):
     """
     caso não exista o contato, cria um
     :return:
     """
-    contato_ = input('Digite o contato que queira adicionar: ')
     contato_ = limpar_texto(contato_)
     if contato_ != '':
         if contato_ not in AGENDA:
-            dados_contatos(contato_)
+            AGENDA[contato_] = {
+                'telefone': telefone,
+                'email': email,
+                'endereco': endereco,
+            }
             print(f'>>> Contato {contato_} Adicionado com Sucesso <<<')
         else:
             print(f'Já existe o contato {contato_}!')
@@ -84,18 +74,22 @@ def incluir_contato():
         print('Digite um contato!')
 
 
-def editar_contato():
+def editar_contato(contato_, telefone, email, endereco):
     """
     caso exista o contato edita os valores
     :return:
     """
-    contato_ = input('Digite o contato que queira editar: ')
+
     contato_ = limpar_texto(contato_)
     if contato_ != '':
         if contato_ not in AGENDA:
             print(f'>>> O Contato {contato_} não existe! <<<')
         else:
-            dados_contatos(contato_)
+            AGENDA[contato_] = {
+                'telefone': telefone,
+                'email': email,
+                'endereco': endereco,
+            }
             print(f'>>> Contato {contato_} editado com Sucesso <<<')
     else:
         print('Digite um contato!')
@@ -114,6 +108,44 @@ def excluir_contato(contato_):
         del AGENDA[contato_]
         print(f'>>> Contato {contato_} Excluido com Sucesso <<<')
 
+
+def exportar_contatos(nome_do_arquivo_):
+    """
+    Exporta os dados da agenda em um arquivo csv
+    :param nome_do_arquivo_:
+    :return:
+    """
+    try:
+        with open(f'{nome_do_arquivo_}.csv', 'w', encoding='utf-8') as agenda:
+            for contato_ in AGENDA:
+                agenda.write(f'{contato_},{AGENDA[contato_]['telefone']},{AGENDA[contato_]['email']},{AGENDA[contato_]['endereco']}\n')
+        print('>>> Agenda exportada com sucesso!! <<<')
+    except Exception as e:
+        print(f'>> ERROR:{e} <<<')
+
+
+def importar_contatos(nome_do_arquivo_):
+    try:
+        with open(f'{nome_do_arquivo_}.csv', 'r', encoding='utf-8') as agenda:
+            contatos = agenda.readlines()
+            for contato_ in contatos:
+                dados = contato_.strip().split(',')
+                nome = dados[0]
+                telefone = dados[1]
+                email = dados[2]
+                endereco = dados[3]
+                incluir_contato(nome, telefone, email, endereco)
+        print(f'>>> Agenda importada com sucesso!! Total de Contatos: {len(AGENDA)} <<<')
+
+    except Exception as e:
+        print(f'>> ERROR:{e} <<<')
+
+
+def salvar():
+    exportar_contatos('database')
+
+def carregar():
+    importar_contatos('database')
 
 def menu():
     """
@@ -134,14 +166,19 @@ MENU DA AGENDA:
 [4] Editar contatos da agenda(caso existam).
 [5] Excluir contatos da agenda.
 
+------------------------------------------------
+[E] Exportar Agenda em CSV
+[I] Importar Contatos
+
 >>> [0] PARA SAIR DO PROGRAMA <<<\n''')
     opcao = input('ESCOLHA 1 OPÇÃO VALIDA(Ex: 1): ')
     return opcao
 
+carregar()
 try:
     while True:
         opcao_ = menu()
-        match opcao_:
+        match opcao_.lower():
             case '1':
                 mostrar_contatos()
             case '2':
@@ -150,18 +187,35 @@ try:
                 if contato != '':
                     buscar_contatos(contato)
             case '3':
-                incluir_contato()
+                contato, telefone, email, endereco = adcionar_dados()
+                incluir_contato(contato,telefone,email,endereco)
             case '4':
-                editar_contato()
+                contato, telefone, email, endereco = adcionar_dados()
+                editar_contato(contato,telefone,email,endereco)
             case '5':
                 contato = input('Digite o contato que deseja excluir: ')
                 contato = limpar_texto(contato)
                 if contato != '':
                     excluir_contato(contato)
+            case 'e':
+                nome_do_arquivo = input('Digite o nome do arquivo: ')
+                exportar_contatos(nome_do_arquivo)
+            case 'i':
+                nome_do_arquivo = input('Digite o nome do arquivo: ')
+                importar_contatos(nome_do_arquivo)
+
             case '0':
                 print('>>> PROGRAMA ENCERRADO <<<')
-                break
+                if AGENDA != {}:
+                    salvar()
+                    break
+                else:
+                    break
             case _:
                 print('>>> DIGITE UMA OPÇÃO VALIDA!! <<<')
 except KeyboardInterrupt:
+    salvar()
     print('\n>>> PROGRAMA ENCERRADO <<<')
+except Exception as e:
+    print(f'>> ERROR:{e} <<<')
+    salvar()
